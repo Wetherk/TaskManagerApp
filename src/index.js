da5841b238d4d652eadef64618060ca9c4fbab9b
Kiryl Ivanou <kiryl.ivanou@sap.com>
2023-01-30 16:42:07 +0100

    create welcome page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("./db/mongoose");
 
 const userRouter = require("./routers/user");
@@ -7,21 +8,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use((err, req, res, next) => {
     res.status(400).send({ error: err.message });
 });
 
-app.get("/live", (req, res) => {
-    res.send("Endpoint is alive!");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/welcome.html"));
 });
 
-app.get("/", (req, res) => {
-    res.send({
-        env: JSON.stringify(process.env),
-    });
+app.get("/live", (req, res) => {
+    res.send("Endpoint is alive!");
 });
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
